559aa78862599c0d34958ebc209e2a3451fa2d48
rusiqe <rusiqe@gmail.com>
2024-02-21 10:49:18 +0100

    shredder exe

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export * from "./navbar";
-export * from "./footer";
-export * from "./layout";
-export * from "./book-card";
-export * from "./feature-card";
-export * from "./category-card";
-export * from "./fixed-plugin";
