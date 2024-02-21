a025119774ee06de6fb544bf6c448037fea279e7
Taurai. M <rusiqe@gmail.com>
2024-02-21 08:58:41 +0000

    change root

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 
-export * from ".src/top-book-categories";
-export * from ".src/hero";
-export * from ".src/layout";
-export * from ".src/page";
-export * from "./srcfaq";
-export * from ".src/other-book-offers";
-export * from ".src/carousel-features";
-export * from ".src/back-to-school-books";
-export * from ".src/get-your-book-from-us";
+export * from "./top-book-categories";
+export * from "./hero";
+export * from "./layout";
+export * from "./page";
+export * from "./faq";
+export * from "./other-book-offers";
+export * from "./carousel-features";
+export * from "./back-to-school-books";
+export * from "./get-your-book-from-us";
 
 
