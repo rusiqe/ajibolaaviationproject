a8545a60ab421569e3aec5fa28e7d4dd4481f15f
Taurai. M <rusiqe@gmail.com>
2024-02-21 08:55:33 +0000

    index copy

---
+++ index.tsx
@@ -0,0 +1,12 @@
+
+export * from ".src/top-book-categories";
+export * from ".src/hero";
+export * from ".src/layout";
+export * from ".src/page";
+export * from "./srcfaq";
+export * from ".src/other-book-offers";
+export * from ".src/carousel-features";
+export * from ".src/back-to-school-books";
+export * from ".src/get-your-book-from-us";
+
+
